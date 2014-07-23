716c543b4b6601aeba05a2689d4c994d490dccc1
Johnathon Britz and Mariana Lenetis <mlenetis@pivotallabs.com>
2014-07-22 17:36:23 -0700

    Removing nonsense test

--- web/handlers/notify_helper_test.go
+++ web/handlers/notify_helper_test.go
@@ -2,11 +2,7 @@ package handlers_test
 
 import (
     . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
 )
 
 var _ = Describe("NotifyHelper", func() {
-    It("does stuff", func() {
-        Expect("lol").To(Equal("lol"))
-    })
 })
