a96ce835085c5259051345b57c951c505f48dd75
Johnathon Britz and Ryan Moran <jbritz@pivotallabs.com>
2014-08-11 13:55:09 -0700

    Moves fakes from registration handler test into init_test file

--- web/handlers/init_test.go
+++ web/handlers/init_test.go
@@ -5,13 +5,10 @@ import (
     "net/http"
     "testing"
 
-    "github.com/cloudfoundry-incubator/notifications/cf"
     "github.com/cloudfoundry-incubator/notifications/config"
-    "github.com/cloudfoundry-incubator/notifications/mail"
+    "github.com/cloudfoundry-incubator/notifications/models"
     "github.com/cloudfoundry-incubator/notifications/postal"
     "github.com/dgrijalva/jwt-go"
-    "github.com/nu7hatch/gouuid"
-    "github.com/pivotal-cf/uaa-sso-golang/uaa"
 
     . "github.com/onsi/ginkgo"
     . "github.com/onsi/gomega"
@@ -71,101 +68,6 @@ func BuildToken(header map[string]interface{}, claims map[string]interface{}) st
     return signed
 }
 
-type FakeMailClient struct {
-    messages       []mail.Message
-    errorOnSend    bool
-    errorOnConnect bool
-}
-
-func (fake *FakeMailClient) Connect() error {
-    if fake.errorOnConnect {
-        return errors.New("BOOM!")
-    }
-    return nil
-}
-
-func (fake *FakeMailClient) Send(msg mail.Message) error {
-    err := fake.Connect()
-    if err != nil {
-        return err
-    }
-
-    if fake.errorOnSend {
-        return errors.New("BOOM!")
-    }
-
-    fake.messages = append(fake.messages, msg)
-    return nil
-}
-
-type FakeUAAClient struct {
-    ClientToken      uaa.Token
-    UsersByID        map[string]uaa.User
-    ErrorForUserByID error
-}
-
-func (fake FakeUAAClient) SetToken(token string) {}
-
-func (fake FakeUAAClient) GetClientToken() (uaa.Token, error) {
-    return fake.ClientToken, nil
-}
-
-func (fake FakeUAAClient) UsersByIDs(ids ...string) ([]uaa.User, error) {
-    users := []uaa.User{}
-    for _, id := range ids {
-        if user, ok := fake.UsersByID[id]; ok {
-            users = append(users, user)
-        }
-    }
-
-    return users, fake.ErrorForUserByID
-}
-
-type FakeCloudController struct {
-    UsersBySpaceGuid         map[string][]cf.CloudControllerUser
-    CurrentToken             string
-    GetUsersBySpaceGuidError error
-    Spaces                   map[string]cf.CloudControllerSpace
-    Orgs                     map[string]cf.CloudControllerOrganization
-}
-
-func NewFakeCloudController() *FakeCloudController {
-    return &FakeCloudController{
-        UsersBySpaceGuid: make(map[string][]cf.CloudControllerUser),
-    }
-}
-
-func (fake *FakeCloudController) GetUsersBySpaceGuid(guid, token string) ([]cf.CloudControllerUser, error) {
-    fake.CurrentToken = token
-
-    if users, ok := fake.UsersBySpaceGuid[guid]; ok {
-        return users, fake.GetUsersBySpaceGuidError
-    } else {
-        return make([]cf.CloudControllerUser, 0), fake.GetUsersBySpaceGuidError
-    }
-}
-
-func (fake *FakeCloudController) LoadSpace(guid, token string) (cf.CloudControllerSpace, error) {
-    if space, ok := fake.Spaces[guid]; ok {
-        return space, nil
-    } else {
-        return cf.CloudControllerSpace{}, nil
-    }
-}
-
-func (fake *FakeCloudController) LoadOrganization(guid, token string) (cf.CloudControllerOrganization, error) {
-    if org, ok := fake.Orgs[guid]; ok {
-        return org, nil
-    } else {
-        return cf.CloudControllerOrganization{}, nil
-    }
-}
-
-var FakeGuidGenerator = postal.GUIDGenerationFunc(func() (*uuid.UUID, error) {
-    guid := uuid.UUID([16]byte{0xDE, 0xAD, 0xBE, 0xEF, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55})
-    return &guid, nil
-})
-
 type FakeCourier struct {
     Error     error
     Responses []postal.Response
@@ -187,4 +89,70 @@ type FakeErrorWriter struct {
 
 func (writer *FakeErrorWriter) Write(w http.ResponseWriter, err error) {
     writer.Error = err
+}
+
+type FakeClientsRepo struct {
+    Clients     map[string]models.Client
+    UpsertError error
+}
+
+func NewFakeClientsRepo() *FakeClientsRepo {
+    return &FakeClientsRepo{
+        Clients: make(map[string]models.Client),
+    }
+}
+
+func (fake *FakeClientsRepo) Create(client models.Client) (models.Client, error) {
+    fake.Clients[client.ID] = client
+    return client, nil
+}
+
+func (fake *FakeClientsRepo) Update(client models.Client) (models.Client, error) {
+    fake.Clients[client.ID] = client
+    return client, nil
+}
+
+func (fake *FakeClientsRepo) Upsert(client models.Client) (models.Client, error) {
+    fake.Clients[client.ID] = client
+    return client, fake.UpsertError
+}
+
+func (fake *FakeClientsRepo) Find(id string) (models.Client, error) {
+    if client, ok := fake.Clients[id]; ok {
+        return client, nil
+    }
+    return models.Client{}, models.ErrRecordNotFound{}
+}
+
+type FakeKindsRepo struct {
+    Kinds       map[string]models.Kind
+    UpsertError error
+}
+
+func NewFakeKindsRepo() *FakeKindsRepo {
+    return &FakeKindsRepo{
+        Kinds: make(map[string]models.Kind),
+    }
+}
+
+func (fake *FakeKindsRepo) Create(kind models.Kind) (models.Kind, error) {
+    fake.Kinds[kind.ID] = kind
+    return kind, nil
+}
+
+func (fake *FakeKindsRepo) Update(kind models.Kind) (models.Kind, error) {
+    fake.Kinds[kind.ID] = kind
+    return kind, nil
+}
+
+func (fake *FakeKindsRepo) Upsert(kind models.Kind) (models.Kind, error) {
+    fake.Kinds[kind.ID] = kind
+    return kind, fake.UpsertError
+}
+
+func (fake *FakeKindsRepo) Find(id string) (models.Kind, error) {
+    if kind, ok := fake.Kinds[id]; ok {
+        return kind, nil
+    }
+    return models.Kind{}, models.ErrRecordNotFound{}
 }
