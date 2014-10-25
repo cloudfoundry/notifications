e0380882ec007149b6e7a03dbdac1008cc2af373
Ian Ornstein and Zachary Gershman <iornstein@pivotal.io>
2014-10-24 17:06:42 -0700

    Added pending acceptance test grabbing templates from DB

---
+++ acceptance/send_overridden_notification_to_user_test.go
@@ -0,0 +1,132 @@
+package acceptance
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strings"
+    "time"
+
+    "github.com/cloudfoundry-incubator/notifications/acceptance/servers"
+    "github.com/cloudfoundry-incubator/notifications/config"
+    "github.com/pivotal-cf/uaa-sso-golang/uaa"
+
+    . "github.com/onsi/ginkgo"
+    . "github.com/onsi/gomega"
+)
+
+var _ = Describe("Send a notification to user with overridden template", func() {
+    BeforeEach(func() {
+        TruncateTables()
+    })
+
+    XIt("send a notification to user", func() {
+        // Boot Fake SMTP Server
+        smtpServer := servers.NewSMTP()
+        smtpServer.Boot()
+
+        // Boot Fake UAA Server
+        uaaServer := servers.NewUAA()
+        uaaServer.Boot()
+        defer uaaServer.Close()
+
+        // Boot Real Notifications Server
+        notificationsServer := servers.NewNotifications()
+        notificationsServer.Boot()
+        defer notificationsServer.Close()
+
+        // Retrieve UAA token
+        env := config.NewEnvironment()
+        uaaClient := uaa.NewUAA("", env.UAAHost, "notifications-sender", "secret", "")
+        clientToken, err := uaaClient.GetClientToken()
+        if err != nil {
+            panic(err)
+        }
+
+        text := "text"
+        html := "<p>html</p>"
+        t := SendOverriddenNotificationToUser{}
+        t.OverrideClientUserTemplate(notificationsServer, clientToken, text, html)
+        t.SendNotificationToUser(notificationsServer, clientToken, smtpServer, text, html)
+    })
+})
+
+type SendOverriddenNotificationToUser struct{}
+
+func (t SendOverriddenNotificationToUser) OverrideClientUserTemplate(notificationsServer servers.Notifications, clientToken uaa.Token, text, html string) {
+    jsonBody := []byte(fmt.Sprintf(`{"text":"%s", "html":"%s"}`, text, html))
+    request, err := http.NewRequest("PUT", notificationsServer.UserTemplateForClientPath("notifications-sender"), bytes.NewBuffer(jsonBody))
+    if err != nil {
+        panic(err)
+    }
+
+    request.Header.Set("Authorization", "Bearer "+clientToken.Access)
+
+    response, err := http.DefaultClient.Do(request)
+    if err != nil {
+        panic(err)
+    }
+
+    // Confirm response status code is a 204
+    Expect(response.StatusCode).To(Equal(http.StatusNoContent))
+}
+func (t SendOverriddenNotificationToUser) SendNotificationToUser(notificationsServer servers.Notifications, clientToken uaa.Token,
+    smtpServer *servers.SMTP, text, html string) {
+
+    body, err := json.Marshal(map[string]string{
+        "kind_id": "acceptance-test",
+        "html":    "<p>this is an acceptance%40test</p>",
+        "subject": "my-special-subject",
+    })
+    if err != nil {
+        panic(err)
+    }
+
+    request, err := http.NewRequest("POST", notificationsServer.UsersPath("user-123"), bytes.NewBuffer(body))
+    if err != nil {
+        panic(err)
+    }
+
+    request.Header.Set("Authorization", "Bearer "+clientToken.Access)
+
+    response, err := http.DefaultClient.Do(request)
+    if err != nil {
+        panic(err)
+    }
+
+    body, err = ioutil.ReadAll(response.Body)
+    if err != nil {
+        panic(err)
+    }
+
+    // Confirm the request response looks correct
+    Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+    responseJSON := []map[string]string{}
+    err = json.Unmarshal(body, &responseJSON)
+    if err != nil {
+        panic(err)
+    }
+
+    Expect(len(responseJSON)).To(Equal(1))
+    responseItem := responseJSON[0]
+    Expect(responseItem["status"]).To(Equal("queued"))
+    Expect(responseItem["recipient"]).To(Equal("user-123"))
+    Expect(GUIDRegex.MatchString(responseItem["notification_id"])).To(BeTrue())
+
+    // Confirm the email message was delivered correctly
+    Eventually(func() int {
+        return len(smtpServer.Deliveries)
+    }, 5*time.Second).Should(Equal(1))
+    delivery := smtpServer.Deliveries[0]
+
+    env := config.NewEnvironment()
+    Expect(delivery.Sender).To(Equal(env.Sender))
+    Expect(delivery.Recipients).To(Equal([]string{"user-123@example.com"}))
+
+    data := strings.Split(string(delivery.Data), "\n")
+    Expect(data).To(ContainElement(text))
+    Expect(data).To(ContainElement(html))
+}
