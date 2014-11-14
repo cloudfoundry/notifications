d6b16e65eb76d5695b77f5fbac9093350a8b97c4
Mariana Lenetis and Zachary Gershman <mlenetis@pivotal.io>
2014-11-14 09:43:46 -0800

    Removes unused code [#82692754]

--- postal/misc.go
+++ postal/misc.go
@@ -11,35 +11,10 @@ const (
 	StatusQueued      = "queued"
 )
 
-type TypedGUID interface {
-	IsTypeEmail() bool
-	String() string
-}
-
 type Templates struct {
 	Subject string
 	Text    string
 	HTML    string
-}
-
-type EmailID string
-
-func (guid EmailID) IsTypeEmail() bool {
-	return true
-}
-
-func (guid EmailID) String() string {
-	return string(guid)
-}
-
-type UAAGUID string
-
-func (guid UAAGUID) IsTypeEmail() bool {
-	return false
-}
-
-func (guid UAAGUID) String() string {
-	return string(guid)
 }
 
 type GUIDGenerationFunc func() (*uuid.UUID, error)
