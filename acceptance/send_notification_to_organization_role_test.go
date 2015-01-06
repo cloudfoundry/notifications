e610c3f18f6adb542aeb5732359ff5e0a8dbd109
Ryan Moran <ryan.moran@gmail.com>
2015-01-05 19:58:49 -0800

    Rewrites notify org role acceptance test to use client

--- acceptance/send_notification_to_organization_role_test.go
+++ acceptance/send_notification_to_organization_role_test.go
@@ -1,13 +1,10 @@
 package acceptance
 
 import (
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"strings"
 	"time"
 
-	"github.com/cloudfoundry-incubator/notifications/acceptance/servers"
 	"github.com/cloudfoundry-incubator/notifications/acceptance/support"
 	"github.com/cloudfoundry-incubator/notifications/application"
 	"github.com/pivotal-cf/uaa-sso-golang/uaa"
@@ -17,289 +14,188 @@ import (
 )
 
 var _ = Describe("Sending notifications to users with certain roles in an organization", func() {
-	It("sends a notification to each user in an organization with that role", func() {
-		clientID := "notifications-sender"
-		clientToken := GetClientTokenFor(clientID)
+	var templateID string
+	var clientID string
+	var clientToken uaa.Token
+	var client *support.Client
 
-		test := SendNotificationsToOrganizationRole{
-			client:              support.NewClient(Servers.Notifications),
-			clientToken:         clientToken,
-			notificationsServer: Servers.Notifications,
-			smtpServer:          Servers.SMTP,
-		}
-		test.RegisterClientNotifications()
-		test.CreateNewTemplate(support.Template{
-			Name:    "ET",
-			Subject: "Phone home {{.Subject}}",
-			HTML:    "<h1>Cat</h1>{{.HTML}}",
-			Text:    "Cat\n{{.Text}}",
+	BeforeEach(func() {
+		clientID = "notifications-sender"
+		clientToken = GetClientTokenFor(clientID)
+		client = support.NewClient(Servers.Notifications)
+		Servers.SMTP.Reset()
+
+		By("registering a notification", func() {
+			status, err := client.Notifications.Register(clientToken.Access, support.RegisterClient{
+				SourceName: "Notifications Sender",
+				Notifications: map[string]support.RegisterNotification{
+					"organization-role-test": {
+						Description: "Organization Role Test",
+					},
+				},
+			})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusNoContent))
 		})
-		test.AssignTemplateToClient(clientID)
-		test.SendNotificationsToOrganizationManagers()
-		test.SendNotificationsToOrganizationAuditors()
-		test.SendNotificationsToOrganizationBillingManagers()
-		test.SendNotificationsToOrganizationInvalidRole()
+
+		By("creating a template", func() {
+			var status int
+			var err error
+			status, templateID, err = client.Templates.Create(clientToken.Access, support.Template{
+				Name:    "ET",
+				Subject: "Phone home {{.Subject}}",
+				HTML:    "<h1>Cat</h1>{{.HTML}}",
+				Text:    "Cat\n{{.Text}}",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusCreated))
+			Expect(templateID).NotTo(Equal(""))
+		})
+
+		By("assigning the template to a client", func() {
+			status, err := client.Templates.AssignToClient(clientToken.Access, clientID, templateID)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusNoContent))
+		})
+	})
+
+	It("sends a notification to each OrgManager in an organization", func() {
+		var response support.NotifyResponse
+
+		By("sending a notification to the OrgManager role", func() {
+			status, responses, err := client.Notify.OrganizationRole(clientToken.Access, "org-123", support.Notify{
+				Role:    "OrgManager",
+				KindID:  "organization-role-test",
+				HTML:    "this is another organization role test",
+				Text:    "this is an organization role test",
+				Subject: "organization-role-subject",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusOK))
+			Expect(responses).To(HaveLen(1))
+
+			response = responses[0]
+			Expect(response.Recipient).To(Equal("user-456"))
+			Expect(response.Status).To(Equal("queued"))
+			Expect(GUIDRegex.MatchString(response.NotificationID)).To(BeTrue())
+		})
+
+		By("confirming the messages were sent", func() {
+			Eventually(func() int {
+				return len(Servers.SMTP.Deliveries)
+			}, 1*time.Second).Should(Equal(1))
+			delivery := Servers.SMTP.Deliveries[0]
+
+			env := application.NewEnvironment()
+			Expect(delivery.Sender).To(Equal(env.Sender))
+			Expect(delivery.Recipients).To(Equal([]string{"user-456@example.com"}))
+
+			data := strings.Split(string(delivery.Data), "\n")
+			Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
+			Expect(data).To(ContainElement("X-CF-Notification-ID: " + response.NotificationID))
+			Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
+			Expect(data).To(ContainElement("Cat"))
+			Expect(data).To(ContainElement("this is an organization role test"))
+			Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
+		})
+	})
+
+	It("sends a notification to each auditor in an organization", func() {
+		var response support.NotifyResponse
+
+		By("sending a notification to the OrgAuditor role", func() {
+			status, responses, err := client.Notify.OrganizationRole(clientToken.Access, "org-123", support.Notify{
+				Role:    "OrgAuditor",
+				KindID:  "organization-role-test",
+				HTML:    "this is another organization role test",
+				Text:    "this is an organization role test",
+				Subject: "organization-role-subject",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusOK))
+			Expect(responses).To(HaveLen(1))
+
+			response = responses[0]
+
+			Expect(response.Recipient).To(Equal("user-123"))
+			Expect(response.Status).To(Equal("queued"))
+			Expect(GUIDRegex.MatchString(response.NotificationID)).To(BeTrue())
+		})
+
+		By("confirming that the messages were sent", func() {
+			Eventually(func() int {
+				return len(Servers.SMTP.Deliveries)
+			}, 1*time.Second).Should(Equal(1))
+			delivery := Servers.SMTP.Deliveries[0]
+
+			env := application.NewEnvironment()
+			Expect(delivery.Sender).To(Equal(env.Sender))
+			Expect(delivery.Recipients).To(Equal([]string{"user-123@example.com"}))
+
+			data := strings.Split(string(delivery.Data), "\n")
+			Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
+			Expect(data).To(ContainElement("X-CF-Notification-ID: " + response.NotificationID))
+			Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
+			Expect(data).To(ContainElement("Cat"))
+			Expect(data).To(ContainElement("this is an organization role test"))
+			Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
+		})
+	})
+
+	It("sends a notification to each billing manager in an organization", func() {
+		var response support.NotifyResponse
+
+		By("sending a notification to the BillingManager role", func() {
+			status, responses, err := client.Notify.OrganizationRole(clientToken.Access, "org-123", support.Notify{
+				Role:    "BillingManager",
+				KindID:  "organization-role-test",
+				HTML:    "this is another organization role test",
+				Text:    "this is an organization role test",
+				Subject: "organization-role-subject",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(http.StatusOK))
+			Expect(responses).To(HaveLen(1))
+
+			response = responses[0]
+
+			Expect(response.Recipient).To(Equal("user-111"))
+			Expect(response.Status).To(Equal("queued"))
+			Expect(GUIDRegex.MatchString(response.NotificationID)).To(BeTrue())
+		})
+
+		By("confirming that the messages were sent", func() {
+			Eventually(func() int {
+				return len(Servers.SMTP.Deliveries)
+			}, 1*time.Second).Should(Equal(1))
+			delivery := Servers.SMTP.Deliveries[0]
+
+			env := application.NewEnvironment()
+			Expect(delivery.Sender).To(Equal(env.Sender))
+			Expect(delivery.Recipients).To(Equal([]string{"user-111@example.com"}))
+
+			data := strings.Split(string(delivery.Data), "\n")
+			Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
+			Expect(data).To(ContainElement("X-CF-Notification-ID: " + response.NotificationID))
+			Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
+			Expect(data).To(ContainElement("Cat"))
+			Expect(data).To(ContainElement("this is an organization role test"))
+			Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
+		})
+	})
+
+	It("sends a notification to an invalid role in an organization", func() {
+		By("sending a notification to an invalid role", func() {
+			status, _, err := client.Notify.OrganizationRole(clientToken.Access, "org-123", support.Notify{
+				Role:    "bad-role",
+				KindID:  "organization-role-test",
+				HTML:    "this is another organization role test",
+				Text:    "this is an organization role test",
+				Subject: "organization-role-subject",
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(status).To(Equal(422))
+		})
 	})
 })
-
-type SendNotificationsToOrganizationRole struct {
-	client              *support.Client
-	clientToken         uaa.Token
-	TemplateID          string
-	notificationsServer servers.Notifications
-	smtpServer          *servers.SMTP
-}
-
-// Make request to /registation
-func (t SendNotificationsToOrganizationRole) RegisterClientNotifications() {
-	code, err := t.client.Notifications.Register(t.clientToken.Access, support.RegisterClient{
-		SourceName: "Notifications Sender",
-		Notifications: map[string]support.RegisterNotification{
-			"organization-role-test": {
-				Description: "Organization Role Test",
-			},
-		},
-	})
-
-	Expect(err).NotTo(HaveOccurred())
-	Expect(code).To(Equal(http.StatusNoContent))
-}
-
-func (t *SendNotificationsToOrganizationRole) CreateNewTemplate(template support.Template) {
-	status, templateID, err := t.client.Templates.Create(t.clientToken.Access, template)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(status).To(Equal(http.StatusCreated))
-	Expect(templateID).NotTo(Equal(""))
-	t.TemplateID = templateID
-}
-
-func (t SendNotificationsToOrganizationRole) AssignTemplateToClient(clientID string) {
-	status, err := t.client.Templates.AssignToClient(t.clientToken.Access, clientID, t.TemplateID)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(status).To(Equal(http.StatusNoContent))
-}
-
-func (t SendNotificationsToOrganizationRole) SendNotificationsToOrganizationManagers() {
-	t.smtpServer.Reset()
-
-	body, err := json.Marshal(map[string]string{
-		"kind_id": "organization-role-test",
-		"html":    "this is another organization role test",
-		"text":    "this is an organization role test",
-		"subject": "organization-role-subject",
-		"role":    "OrgManager",
-	})
-	request, err := http.NewRequest("POST", t.notificationsServer.OrganizationsPath("org-123"), bytes.NewBuffer(body))
-	if err != nil {
-		panic(err)
-	}
-
-	request.Header.Set("Authorization", "Bearer "+t.clientToken.Access)
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		panic(err)
-	}
-
-	// Confirm the request response looks correct
-	Expect(response.StatusCode).To(Equal(http.StatusOK))
-
-	responseJSON := []map[string]string{}
-	err = json.NewDecoder(response.Body).Decode(&responseJSON)
-	if err != nil {
-		panic(err)
-	}
-
-	Expect(len(responseJSON)).To(Equal(1))
-
-	indexedResponses := map[string]map[string]string{}
-	for _, resp := range responseJSON {
-		indexedResponses[resp["recipient"]] = resp
-	}
-
-	responseItem := indexedResponses["user-456"]
-	Expect(responseItem["recipient"]).To(Equal("user-456"))
-	Expect(responseItem["status"]).To(Equal("queued"))
-	Expect(GUIDRegex.MatchString(responseItem["notification_id"])).To(BeTrue())
-
-	// Confirm the email message was delivered correctly
-	Eventually(func() int {
-		return len(t.smtpServer.Deliveries)
-	}, 1*time.Second).Should(Equal(1))
-	delivery := t.smtpServer.Deliveries[0]
-
-	env := application.NewEnvironment()
-	Expect(delivery.Sender).To(Equal(env.Sender))
-	Expect(delivery.Recipients).To(Equal([]string{"user-456@example.com"}))
-
-	data := strings.Split(string(delivery.Data), "\n")
-	Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
-	Expect(data).To(ContainElement("X-CF-Notification-ID: " + indexedResponses["user-456"]["notification_id"]))
-	Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
-	Expect(data).To(ContainElement("Cat"))
-	Expect(data).To(ContainElement("this is an organization role test"))
-	Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
-}
-
-// Make request to /organization/:guid for auditors
-func (t SendNotificationsToOrganizationRole) SendNotificationsToOrganizationAuditors() {
-	t.smtpServer.Reset()
-
-	body, err := json.Marshal(map[string]string{
-		"kind_id": "organization-role-test",
-		"html":    "this is another organization role test",
-		"text":    "this is an organization role test",
-		"subject": "organization-role-subject",
-		"role":    "OrgAuditor",
-	})
-	request, err := http.NewRequest("POST", t.notificationsServer.OrganizationsPath("org-123"), bytes.NewBuffer(body))
-	if err != nil {
-		panic(err)
-	}
-
-	request.Header.Set("Authorization", "Bearer "+t.clientToken.Access)
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		panic(err)
-	}
-
-	// Confirm the request response looks correct
-	Expect(response.StatusCode).To(Equal(http.StatusOK))
-
-	responseJSON := []map[string]string{}
-	err = json.NewDecoder(response.Body).Decode(&responseJSON)
-	if err != nil {
-		panic(err)
-	}
-
-	Expect(len(responseJSON)).To(Equal(1))
-
-	indexedResponses := map[string]map[string]string{}
-	for _, resp := range responseJSON {
-		indexedResponses[resp["recipient"]] = resp
-	}
-
-	responseItem := indexedResponses["user-123"]
-	Expect(responseItem["recipient"]).To(Equal("user-123"))
-	Expect(responseItem["status"]).To(Equal("queued"))
-	Expect(GUIDRegex.MatchString(responseItem["notification_id"])).To(BeTrue())
-
-	// Confirm the email message was delivered correctly
-	Eventually(func() int {
-		return len(t.smtpServer.Deliveries)
-	}, 1*time.Second).Should(Equal(1))
-	delivery := t.smtpServer.Deliveries[0]
-
-	env := application.NewEnvironment()
-	Expect(delivery.Sender).To(Equal(env.Sender))
-	Expect(delivery.Recipients).To(Equal([]string{"user-123@example.com"}))
-
-	data := strings.Split(string(delivery.Data), "\n")
-	Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
-	Expect(data).To(ContainElement("X-CF-Notification-ID: " + indexedResponses["user-123"]["notification_id"]))
-	Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
-	Expect(data).To(ContainElement("Cat"))
-	Expect(data).To(ContainElement("this is an organization role test"))
-	Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
-}
-
-// Make request to /organization/:guid for billing managers
-func (t SendNotificationsToOrganizationRole) SendNotificationsToOrganizationBillingManagers() {
-	t.smtpServer.Reset()
-
-	body, err := json.Marshal(map[string]string{
-		"kind_id": "organization-role-test",
-		"html":    "this is another organization role test",
-		"text":    "this is an organization role test",
-		"subject": "organization-role-subject",
-		"role":    "BillingManager",
-	})
-	request, err := http.NewRequest("POST", t.notificationsServer.OrganizationsPath("org-123"), bytes.NewBuffer(body))
-	if err != nil {
-		panic(err)
-	}
-
-	request.Header.Set("Authorization", "Bearer "+t.clientToken.Access)
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		panic(err)
-	}
-
-	// Confirm the request response looks correct
-	Expect(response.StatusCode).To(Equal(http.StatusOK))
-
-	responseJSON := []map[string]string{}
-	err = json.NewDecoder(response.Body).Decode(&responseJSON)
-	if err != nil {
-		panic(err)
-	}
-
-	Expect(len(responseJSON)).To(Equal(1))
-
-	indexedResponses := map[string]map[string]string{}
-	for _, resp := range responseJSON {
-		indexedResponses[resp["recipient"]] = resp
-	}
-
-	responseItem := indexedResponses["user-111"]
-	Expect(responseItem["recipient"]).To(Equal("user-111"))
-	Expect(responseItem["status"]).To(Equal("queued"))
-	Expect(GUIDRegex.MatchString(responseItem["notification_id"])).To(BeTrue())
-
-	// Confirm the email message was delivered correctly
-	Eventually(func() int {
-		return len(t.smtpServer.Deliveries)
-	}, 1*time.Second).Should(Equal(1))
-	delivery := t.smtpServer.Deliveries[0]
-
-	env := application.NewEnvironment()
-	Expect(delivery.Sender).To(Equal(env.Sender))
-	Expect(delivery.Recipients).To(Equal([]string{"user-111@example.com"}))
-
-	data := strings.Split(string(delivery.Data), "\n")
-	Expect(data).To(ContainElement("X-CF-Client-ID: notifications-sender"))
-	Expect(data).To(ContainElement("X-CF-Notification-ID: " + indexedResponses["user-111"]["notification_id"]))
-	Expect(data).To(ContainElement("Subject: Phone home organization-role-subject"))
-	Expect(data).To(ContainElement("Cat"))
-	Expect(data).To(ContainElement("this is an organization role test"))
-	Expect(data).To(ContainElement("        <h1>Cat</h1>this is another organization role test"))
-}
-
-// Make request to /organization/:guid for invalid role
-func (t SendNotificationsToOrganizationRole) SendNotificationsToOrganizationInvalidRole() {
-	t.smtpServer.Reset()
-
-	body, err := json.Marshal(map[string]string{
-		"kind_id": "organization-role-test",
-		"html":    "this is another organization role test",
-		"text":    "this is an organization role test",
-		"subject": "organization-role-subject",
-		"role":    "bad-role",
-	})
-	request, err := http.NewRequest("POST", t.notificationsServer.OrganizationsPath("org-123"), bytes.NewBuffer(body))
-	if err != nil {
-		panic(err)
-	}
-
-	request.Header.Set("Authorization", "Bearer "+t.clientToken.Access)
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		panic(err)
-	}
-
-	// Confirm the request response looks correct
-	Expect(response.StatusCode).To(Equal(422))
-
-	responseJSON := map[string][]string{}
-	err = json.NewDecoder(response.Body).Decode(&responseJSON)
-	if err != nil {
-		panic(err)
-	}
-
-	Expect(responseJSON).To(Equal(map[string][]string{
-		"errors": {`"role" must be "OrgManager", "OrgAuditor", "BillingManager" or unset`},
-	}))
-}
--- acceptance/support/notify_service.go
+++ acceptance/support/notify_service.go
@@ -3,6 +3,7 @@ package support
 import (
 	"bytes"
 	"encoding/json"
+	"net/http"
 )
 
 type NotifyService struct {
@@ -15,6 +16,7 @@ type Notify struct {
 	Subject string `json:"subject"`
 	Text    string `json:"text"`
 	To      string `json:"to,omitempty"`
+	Role    string `json:"role,omitempty"`
 }
 
 type NotifyResponse struct {
@@ -23,7 +25,7 @@ type NotifyResponse struct {
 	NotificationID string `json:"notification_id"`
 }
 
-func (n NotifyService) User(token, userID string, notify Notify) (int, []NotifyResponse, error) {
+func (n NotifyService) User(token, userGUID string, notify Notify) (int, []NotifyResponse, error) {
 	var responses []NotifyResponse
 
 	body, err := json.Marshal(notify)
@@ -31,7 +33,7 @@ func (n NotifyService) User(token, userID string, notify Notify) (int, []NotifyR
 		return 0, responses, err
 	}
 
-	status, responseBody, err := n.client.makeRequest("POST", n.client.server.UsersPath(userID), bytes.NewBuffer(body), token)
+	status, responseBody, err := n.client.makeRequest("POST", n.client.server.UsersPath(userGUID), bytes.NewBuffer(body), token)
 	if err != nil {
 		return 0, responses, err
 	}
@@ -81,6 +83,29 @@ func (n NotifyService) Email(token string, notify Notify) (int, []NotifyResponse
 	err = json.NewDecoder(responseBody).Decode(&responses)
 	if err != nil {
 		return 0, responses, err
+	}
+
+	return status, responses, nil
+}
+
+func (n NotifyService) OrganizationRole(token, organizationGUID string, notify Notify) (int, []NotifyResponse, error) {
+	var responses []NotifyResponse
+
+	body, err := json.Marshal(notify)
+	if err != nil {
+		return 0, responses, err
+	}
+
+	status, responseBody, err := n.client.makeRequest("POST", n.client.server.OrganizationsPath(organizationGUID), bytes.NewBuffer(body), token)
+	if err != nil {
+		return 0, responses, err
+	}
+
+	if status == http.StatusOK {
+		err = json.NewDecoder(responseBody).Decode(&responses)
+		if err != nil {
+			return 0, responses, err
+		}
 	}
 
 	return status, responses, nil
