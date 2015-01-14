6b7b5a4f1450c115a173646fc110d634420638d1
Ian Ornstein and Zachary Gershman <iornstein@pivotal.io>
2015-01-13 17:53:07 -0800

    Updates migration because Travis pukes on it

--- models/migrations/14_add_updated_at_to_kinds.sql
+++ models/migrations/14_add_updated_at_to_kinds.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE `kinds` ADD `updated_at` datetime DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `kinds` ADD `updated_at` datetime DEFAULT '2000-01-01 00:00:00';
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
