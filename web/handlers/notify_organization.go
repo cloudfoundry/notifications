36e105b12e05d4501f41f7241a5f45f85f1fff62
Ryan Moran and Zachary Gershman <zgershman@pivotal.io>
2015-05-20 14:19:02 -0700

    Converts notify organization handler to use db from allocator
    [#93652080]

--- web/handlers/notify_organization.go
+++ web/handlers/notify_organization.go
@@ -14,41 +14,27 @@ type NotifyOrganization struct {
 	errorWriter ErrorWriterInterface
 	notify      NotifyInterface
 	strategy    strategies.StrategyInterface
-	database    models.DatabaseInterface
 }
 
-func NewNotifyOrganization(notify NotifyInterface, errorWriter ErrorWriterInterface, strategy strategies.StrategyInterface, database models.DatabaseInterface) NotifyOrganization {
+func NewNotifyOrganization(notify NotifyInterface, errorWriter ErrorWriterInterface, strategy strategies.StrategyInterface) NotifyOrganization {
 	return NotifyOrganization{
 		errorWriter: errorWriter,
 		notify:      notify,
 		strategy:    strategy,
-		database:    database,
 	}
 }
 
 func (handler NotifyOrganization) ServeHTTP(w http.ResponseWriter, req *http.Request, context stack.Context) {
-	connection := handler.database.Connection()
-	err := handler.Execute(w, req, connection, context)
-	if err != nil {
-		handler.errorWriter.Write(w, err)
-		return
-	}
-}
-
-func (handler NotifyOrganization) Execute(w http.ResponseWriter, req *http.Request, connection models.ConnectionInterface,
-	context stack.Context) error {
-
+	connection := context.Get("database").(models.DatabaseInterface).Connection()
 	organizationGUID := strings.TrimPrefix(req.URL.Path, "/organizations/")
-
 	vcapRequestID := context.Get(VCAPRequestIDKey).(string)
 
 	output, err := handler.notify.Execute(connection, req, context, organizationGUID, handler.strategy, params.GUIDValidator{}, vcapRequestID)
 	if err != nil {
-		return err
+		handler.errorWriter.Write(w, err)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(output)
-
-	return nil
 }
