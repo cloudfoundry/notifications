7c375a53b19858e1e4194bd421a195cfb8e02726
Ryan Moran and Zachary Gershman <rmoran@pivotal.io>
2015-05-20 14:01:24 -0700

    Converts notify uaa scope handler to use db allocator
    [#93652080]

--- web/handlers/notify_uaa_scope.go
+++ web/handlers/notify_uaa_scope.go
@@ -14,38 +14,27 @@ type NotifyUAAScope struct {
 	errorWriter ErrorWriterInterface
 	notify      NotifyInterface
 	strategy    strategies.StrategyInterface
-	database    models.DatabaseInterface
 }
 
-func NewNotifyUAAScope(notify NotifyInterface, errorWriter ErrorWriterInterface, strategy strategies.StrategyInterface, database models.DatabaseInterface) NotifyUAAScope {
+func NewNotifyUAAScope(notify NotifyInterface, errorWriter ErrorWriterInterface, strategy strategies.StrategyInterface) NotifyUAAScope {
 	return NotifyUAAScope{
 		errorWriter: errorWriter,
 		notify:      notify,
 		strategy:    strategy,
-		database:    database,
 	}
 }
 
 func (handler NotifyUAAScope) ServeHTTP(w http.ResponseWriter, req *http.Request, context stack.Context) {
-	connection := handler.database.Connection()
-	err := handler.Execute(w, req, connection, context)
-	if err != nil {
-		handler.errorWriter.Write(w, err)
-		return
-	}
-}
-
-func (handler NotifyUAAScope) Execute(w http.ResponseWriter, req *http.Request, connection models.ConnectionInterface, context stack.Context) error {
+	connection := context.Get("database").(models.DatabaseInterface).Connection()
 	scope := strings.TrimPrefix(req.URL.Path, "/uaa_scopes/")
 	vcapRequestID := context.Get(VCAPRequestIDKey).(string)
 
 	output, err := handler.notify.Execute(connection, req, context, scope, handler.strategy, params.GUIDValidator{}, vcapRequestID)
 	if err != nil {
-		return err
+		handler.errorWriter.Write(w, err)
+		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(output)
-
-	return nil
 }
--- web/handlers/notify_uaa_scope_test.go
+++ web/handlers/notify_uaa_scope_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"reflect"
 
 	"github.com/cloudfoundry-incubator/notifications/fakes"
 	"github.com/cloudfoundry-incubator/notifications/web/handlers"
@@ -18,41 +19,47 @@ import (
 var _ = Describe("NotifyUAAScope", func() {
 	Describe("Execute", func() {
 		var (
-			notify     *fakes.Notify
-			handler    handlers.NotifyUAAScope
-			writer     *httptest.ResponseRecorder
-			request    *http.Request
-			context    stack.Context
-			connection *fakes.DBConn
-			strategy   *fakes.Strategy
+			notify      *fakes.Notify
+			handler     handlers.NotifyUAAScope
+			writer      *httptest.ResponseRecorder
+			request     *http.Request
+			context     stack.Context
+			connection  *fakes.DBConn
+			errorWriter *fakes.ErrorWriter
+			strategy    *fakes.Strategy
 		)
 
 		BeforeEach(func() {
 			writer = httptest.NewRecorder()
 			request = &http.Request{URL: &url.URL{Path: "/uaa_scopes/great.scope"}}
 			strategy = fakes.NewStrategy()
+			connection = fakes.NewDBConn()
+			database := fakes.NewDatabase()
+			database.Conn = connection
+			errorWriter = fakes.NewErrorWriter()
 
 			context = stack.NewContext()
+			context.Set("database", database)
 			context.Set(handlers.VCAPRequestIDKey, "some-request-id")
 
 			notify = fakes.NewNotify()
-			handler = handlers.NewNotifyUAAScope(notify, nil, strategy, nil)
+			handler = handlers.NewNotifyUAAScope(notify, errorWriter, strategy)
 		})
 
 		Context("when the notify.Execute returns a successful response", func() {
 			It("returns the JSON representation of the response", func() {
 				notify.ExecuteCall.Response = []byte("whatever")
 
-				handler.Execute(writer, request, connection, context)
+				handler.ServeHTTP(writer, request, context)
 
 				Expect(writer.Code).To(Equal(http.StatusOK))
 				Expect(writer.Body.String()).To(Equal("whatever"))
 			})
 
 			It("delegates to the Notify object with the correct arguments", func() {
-				handler.Execute(writer, request, connection, context)
+				handler.ServeHTTP(writer, request, context)
 
-				Expect(notify.ExecuteCall.Args.Connection).To(Equal(connection))
+				Expect(reflect.ValueOf(notify.ExecuteCall.Args.Connection).Pointer()).To(Equal(reflect.ValueOf(connection).Pointer()))
 				Expect(notify.ExecuteCall.Args.Request).To(Equal(request))
 				Expect(notify.ExecuteCall.Args.Context).To(Equal(context))
 				Expect(notify.ExecuteCall.Args.GUID).To(Equal("great.scope"))
@@ -66,8 +73,8 @@ var _ = Describe("NotifyUAAScope", func() {
 			It("Propagates the error", func() {
 				notify.ExecuteCall.Error = errors.New("the error")
 
-				err := handler.Execute(writer, request, connection, context)
-				Expect(err).To(Equal(notify.ExecuteCall.Error))
+				handler.ServeHTTP(writer, request, context)
+				Expect(errorWriter.Error).To(Equal(notify.ExecuteCall.Error))
 			})
 		})
 	})
