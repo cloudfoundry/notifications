10971ad37aebef24f17d267e9124997cda7bac47
Ryan Moran <rmoran@pivotal.io>
2015-08-13 16:36:37 -0700

    Fixes campaign type route for show endpoint to handle trailing slash
    [#99834174]
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- v2/web/campaigntypes/routes.go
+++ v2/web/campaigntypes/routes.go
@@ -20,7 +20,7 @@ type Routes struct {
 func (r Routes) Register(m muxer) {
 	m.Handle("POST", "/senders/{sender_id}/campaign_types", NewCreateHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
 	m.Handle("GET", "/senders/{sender_id}/campaign_types", NewListHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
-	m.Handle("GET", "/senders/{sender_id}/campaign_types/{campaign_type_id}", NewShowHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
+	m.Handle("GET", "/senders/{sender_id}/campaign_types/{campaign_type_id:.*}", NewShowHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
 	m.Handle("PUT", "/senders/{sender_id}/campaign_types/{campaign_type_id}", NewUpdateHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
 	m.Handle("DELETE", "/senders/{sender_id}/campaign_types/{campaign_type_id}", NewDeleteHandler(r.CampaignTypesCollection), r.RequestLogging, r.Authenticator, r.DatabaseAllocator)
 }
