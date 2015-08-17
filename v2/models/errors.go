299ad9845cdc9b167581d0864be90384c22603a6
Dave Walter <dwalter@pivotal.io>
2015-08-17 14:59:12 -0700

    Removed unused error structs.
    Signed-off-by: Evan Farrar <efarrar@pivotal.io>

--- v2/models/errors.go
+++ v2/models/errors.go
@@ -21,33 +21,3 @@ type DuplicateRecordError struct {
 func (err DuplicateRecordError) Error() string {
 	return err.Err.Error()
 }
-
-type TransactionCommitError struct {
-	message string
-}
-
-func NewTransactionCommitError(msg string) TransactionCommitError {
-	return TransactionCommitError{
-		message: msg,
-	}
-}
-
-func (err TransactionCommitError) Error() string {
-	return err.message
-}
-
-type TemplateFindError struct {
-	Message string
-}
-
-func (err TemplateFindError) Error() string {
-	return err.Message
-}
-
-type TemplateUpdateError struct {
-	Message string
-}
-
-func (err TemplateUpdateError) Error() string {
-	return err.Message
-}
